3327bce73d618130b7d3b3d4f1320b49d5db0ea9
MechelleSieglitz <mechelle.sieglitz@gmail.com>
2022-05-13 10:24:35 -0400

    Git init -- custom 2 choice dialog

---
+++ lib/choice_dialog.dart
@@ -0,0 +1,62 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ChoiceDialog extends StatelessWidget {
+  final String? title;
+  final String? message;
+  final String? firstButtonText;
+  final VoidCallback? firstButtonOnPress;
+  final String? secondButtonText;
+  final VoidCallback? secondButtonOnPress;
+
+  ChoiceDialog({this.title,
+    this.message,
+    this.firstButtonText,
+    required this.firstButtonOnPress,
+    this.secondButtonText,
+    required this.secondButtonOnPress});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20)
+      ),
+      title: Text(
+        title ?? '',
+      ),
+      content: Text(
+        message ?? '',
+      ),
+      actions: [
+        TextButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.white),
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(
+              side: BorderSide(color: Colors.deepOrangeAccent),
+              borderRadius: BorderRadius.circular(20),
+
+            )
+          ),),
+            onPressed: () {
+              firstButtonOnPress!();
+              Navigator.of(context).pop();
+            },
+            child: Text(firstButtonText!, style: TextStyle(color: Colors.deepOrangeAccent))),
+        TextButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.deepOrangeAccent),
+              shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              )
+              ),),
+            onPressed: () {
+              secondButtonOnPress!();
+              Navigator.of(context).pop();
+            },
+            child: Text(secondButtonText!, style: TextStyle(color: Colors.white),))
+      ],
+    );
+  }
+}
